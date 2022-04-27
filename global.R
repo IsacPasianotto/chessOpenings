cbc6ec0c16fe78461d3703c5e200cc651fcb0f63
Isac Pasianotto <yh4mop12z@gmail.com>
2022-04-27 14:13:38 +0000

    create global.R

---
+++ global.R
@@ -0,0 +1,17 @@
+library(shiny)
+library(shinyjs)
+library(rchess)
+library(stringr)
+library(tools)
+library(ggplot2)
+
+
+
+# COMING SOON ... 
+
+my_theme <- function() {
+  # theme(
+  # ...
+  # ) + 
+  # theme_light()
+}
